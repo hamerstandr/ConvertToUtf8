d39729490ba267386f9e1d0f005323dc8a31481a
hamerstandr <hamerstandr@yahoo.com>
2019-03-25 04:40:12 -0700

    New

---
+++ ConvertToUtf8/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ConvertToUtf8
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string Arg="";
+        public App()
+        {
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args != null)
+            {
+                if (e.Args.Length > 0)
+                {
+                    for (int i = 0; i < e.Args.Length; i++)
+                        Arg += e.Args[i];
+                }
+            }
+        }
+    }
+}
---
+++ ConvertToUtf8/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace ConvertToUtf8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    { string str = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (App.Arg != "")
+            {
+                try
+                {
+                    var arabic = Encoding.GetEncoding(1256);
+                    StreamReader reader = new StreamReader(App.Arg, arabic);
+                    str = reader.ReadToEnd();
+                    reader.Close();
+                    TextBox1.Text = str;
+                }
+                catch { }
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try {/* File.Delete(App.Arg); File.CreateText(App.Arg, true, Encoding.UTF8);*/ } catch { }
+            try {
+                Stream s =File.Create(App.Arg);
+                StreamWriter writer =  new StreamWriter(s, Encoding.UTF8);
+                writer.Write(str);
+                writer.Close();
+                TextMasege.Text = "Saved.";
+                TextMasege.Foreground = Brushes.DarkGreen;
+            } catch { }
+        }
+    }
+}
